7e73650fdc7f757934313d833b7a8e16539e6b1f
Kirankumar283 <devkiranprajapati@gmail.com>
2025-07-03 21:30:48 +0530

    basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello YASH Here🤸👈</h1>
+      <h1 className="flex align-center justify-center text-3xl font-bold">
+        Hello YASH Here🤸👈
+      </h1>
     </div>
   );
 };
