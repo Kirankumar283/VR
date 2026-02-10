a86b52a11a935cf4b7212c7421a5724fb8944c10
Kiran Prajapati <devkiranprajapati@gmail.com>
2026-02-10 14:28:45 +0530

    Add welcome message to App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ const App = () => {
       <h1 className="flex align-center justify-center text-3xl font-bold">
         Hello KP Here🤸👈
       </h1>
+      <h3 className="flex align-center justify-center text-2xl font-bold">
+        Welcome to Virtual Reality! 🚀
+      </h3>
     </div>
   );
 };
