e9e0c6cbc8d47e073af40641668a2ed5815311e7
Kiran Prajapati <devkiranprajapati@gmail.com>
2026-02-11 18:00:37 +0530

    Added basic navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="flex align-center justify-center text-3xl font-bold">
-        Hello KP Here🤸👈
-      </h1>
-      <h3 className="flex align-center justify-center text-2xl font-bold">
-        Welcome to Virtual Reality! 🚀
-      </h3>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 };
 
